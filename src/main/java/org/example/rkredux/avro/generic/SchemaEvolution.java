cc3136db2b8375d2d3955e2e12d47141954adf7d
rkumar <rkredux@gmail.com>
2020-09-06 14:29:38 -0400

    schema evolution corrections

--- src/main/java/org/example/rkredux/avro/generic/SchemaEvolution.java
+++ src/main/java/org/example/rkredux/avro/generic/SchemaEvolution.java
@@ -4,17 +4,17 @@ import com.example.Customer;
 import com.example.CustomerV2;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
-import org.apache.avro.specific.SpecificRecord;
-
 import java.io.File;
 import java.io.IOException;
 
 public class SchemaEvolution{
 
     public static void main(String[] args) {
-//        writing the record
+        //record serialization
         Customer.Builder builder = Customer.newBuilder();
         builder.setFirstName("Rahul");
         builder.setLastName("Kumar");
@@ -24,25 +24,30 @@ public class SchemaEvolution{
         builder.setAutomatedEmail(false);
         final Customer customerV1record = builder.build();
 
-        SpecificDatumWriter specificCustomerV1DatumWriter = new SpecificDatumWriter(Customer.class);
-        DataFileWriter<Customer> dataFileWriter = new DataFileWriter<>(specificCustomerV1DatumWriter);
+        DatumWriter<Customer> customerV1DatumWriter = new SpecificDatumWriter<>(Customer.class);
+        DataFileWriter<Customer> dataFileWriter = new DataFileWriter<>(customerV1DatumWriter);
         try {
             dataFileWriter.create(customerV1record.getSchema(), new File("customerV1-specific.avro"));
             dataFileWriter.append(customerV1record);
+            dataFileWriter.close();
             System.out.println(String.format("Successfully written avro record with schema %s to the file", customerV1record.getSchema().getName()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        reading the records
+
+        //deserialization
         final File file = new File("customerV1-specific.avro");
-        SpecificDatumReader<CustomerV2> recordSpecificDatumReader = new SpecificDatumReader<>(CustomerV2.class);
+        DatumReader<CustomerV2> customerV2DatumReader = new SpecificDatumReader<>(CustomerV2.class);
         try {
             DataFileReader<CustomerV2> dataFileReader;
-            dataFileReader = new DataFileReader<>(file, recordSpecificDatumReader);
+            dataFileReader = new DataFileReader<>(file, customerV2DatumReader);
+            CustomerV2 customerV2 = null;
             while (dataFileReader.hasNext()){
-                System.out.println("Starting reading file");
-                CustomerV2 readCustomer = dataFileReader.next();
-                System.out.println(readCustomer);
+                System.out.println("About to read");
+                customerV2 = dataFileReader.next(customerV2);
+                System.out.println(customerV2);
+                System.out.println(customerV2.getFirstName());
+                System.out.println(String.format("The customer's first name is %s and last name is %s", customerV2.getFirstName(), customerV2.getLastName()));
             }
         } catch (IOException e) {
             e.printStackTrace();
